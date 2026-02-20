299c7bb21628d777ed4759130c14681ef07e3531
Marcos Luciano Gallardo Baez <marcos.gallardo.baez@gmail.com>
2026-02-20 18:32:34 -0300

    chore: resolve merge conflicts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
 import { sequelize } from './config/db.config'
 import { errorHandler } from './middleware/errorHandler'
 
+import { runSeeds } from './seed'
+import { RiderRoutes } from './routes/rider.routes'
 import { AppRoutes } from './routes/app.routes'
 import { setupSwagger } from './config/swagger.config'
 
@@ -17,12 +19,17 @@ const routes = AppRoutes.routes
 
 setupSwagger(app)
 
+//Routes
 app.use('/api/v1/', routes)
+app.use('/api/v1/riders', RiderRoutes.routes )
 
 const main = async () => {
   try {
     await sequelize.sync({ alter: true })
     console.log('Database synced')
+
+    await runSeeds()
+
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
     })
