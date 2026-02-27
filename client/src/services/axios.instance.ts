e0cd57c6b78b4697fb68e8981e98ca63c5f90874
Danna Elizabeth <dannaelizabethrb@gmail.com>
2026-02-27 15:57:24 -0400

    [ADD] Auth & Segurity

--- client/src/services/axios.instance.ts
+++ client/src/services/axios.instance.ts
@@ -1,11 +1,17 @@
 import axios from "axios";
 
+const getCookie = (name: string) => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(';').shift();
+}
+
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.PUBLIC_API_URL || "http://localhost:3000/api/v1",
 })
 
 axiosInstance.interceptors.request.use((config) =>{
-    const token = localStorage.getItem('token')
+    const token = getCookie('token')
     if (token){
         config.headers.Authorization = `Bearer ${token}`
     }
